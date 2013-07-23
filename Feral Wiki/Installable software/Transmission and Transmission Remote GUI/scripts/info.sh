da860f09a6a4eb814f825b13f7eba7049c955bc9
randomessence <random@digikool.co.uk>
2013-07-23 20:07:13 +0100

    info.sh

--- Feral Wiki/Installable software/Transmission and Transmission Remote GUI/scripts/info.sh
+++ Feral Wiki/Installable software/Transmission and Transmission Remote GUI/scripts/info.sh
@@ -8,6 +8,7 @@ then
     echo -e "Your hostname is:" "\033[33m""$(hostname)""\e[0m"
     echo -e "Your username is:" "\033[32m""$(whoami)""\e[0m"
     echo -e "Your RPC port is:" "\033[31m""$(sed -rn 's/(.*)"rpc-port":(\s+)(.*),/\3/p' ~/.config/transmission-daemon/settings.json)""\e[0m"
+    echo
     echo -e "You can find your password in your" "\033[32m""Account Manager""\e[0m" "on the" "\033[31m""Slot Details""\e[0m" "page for that slot under Transmission"
     echo -e "\033[33m""Your password is the same as the Transmission Web Gui password.""\e[0m"
     echo
