8227fa8f909c859c38fa9b52bf60be643bae9264
randomessence <random@digikool.co.uk>
2013-08-12 22:19:36 +0100

    multirtru

---
+++ Feral Wiki/Software/Multiple instances - rtorrent and rutorrent/readme.md
@@ -0,0 +1,158 @@
+
+### Running multiple instances of rtorrent and rutorrent
+
+**Important note:** Please make sure you have installed rtorrent/rutorrent using the [**Install Software** link in your Manager](https://www.feralhosting.com/manager/)
+
+This is quite easy to do once you have understood the basic idea. We will:
+
+**1:** Clone the existing installation giving it a new name.
+**2:** We edit some files to reflect the new paths.
+**3:** We run our new instance in a custom named screen.
+**4:** Visit the new Web Gui in your WWW and use it.
+**5:** Repeat as many times as needed.
+
+### Clone our existing installation
+
+**1:** Clone our `~/rtorrent.rc`
+
+~~~
+cp -f ~/.rtorrent.rc ~/.rtorrent-1.rc
+~~~
+
+**2:** Clone the rutorrent Web Gui
+
+~~~
+cp -rf ~/www/$(whoami).$(hostname)/public_html/rutorrent/. ~/www/$(whoami).$(hostname)/public_html/rutorrent-1
+~~~
+
+**3:** Create some directories for our new instance.
+
+~~~
+mkdir -p ~/private/rtorrent-1/data ~/private/rtorrent-1/watch ~/private/rtorrent-1/work
+~~~
+
+**Important note:** Take note of the suffix `-1` used in all cases. rtorrent to rtorrent-1 and rutorrent to rutorrent-1.
+
+### Edit the files - rtorrent
+
+We must now make some edits to these files so that our new rtorrent and rutorrent will work together.
+
+~~~
+~/rtorrent-1.rc
+~~~
+~~~
+~/www/username.feralhosting.com/public_html/rutorrent-1/conf/config.php
+~~~
+
+**Important note:** EOL is important for this file, it must be UNIX/LF if using a text editor over FTP. Please see this FAQ: [Text editing - Over FTP or SFTP](https://www.feralhosting.com/faq/view?question=219)
+
+Open the `~/rtorrent-1.rc`:
+
+~~~
+nano -w ~/.rtorrent-1.rc
+~~~
+
+Edit these 5 lines in you `~/rtorrent-1.rc`, changing the paths to reflect the new locations with the new suffix. This FAQ uses the examples of `-1`:
+
+~~~
+directory = media/DiskID/home/username/private/rtorrent/data
+session = media/DiskID/home/username/private/rtorrent/work
+schedule = watch_directory, 5, 60, load_start=media/DiskID/home/username/private/rtorrent/watch/*.torrent
+execute = {sh,-c,/usr/bin/php media/DiskID/home/username/www/username.server.feralhosting.com/public_html/rutorrent/php/initplugins.php username &}
+scgi_local = media/DiskID/home/username/private/rtorrent/.socket
+~~~
+
+For this example we make these changes:
+
+`rtorrent` to `rtorrent-1`
+
+~~~
+directory = media/DiskID/home/username/private/rtorrent-1/data
+~~~
+
+`rtorrent` to `rtorrent-1`
+
+~~~
+session = media/DiskID/home/username/private/rtorrent-1/work
+~~~
+
+`rtorrent` to `rtorrent-1`
+
+~~~
+schedule = watch_directory, 5, 60, load_start=media/DiskID/home/username/private/rtorrent-1/watch/*.torrent
+~~~
+
+`rutorrent` to `rutorrent-1`
+
+~~~
+execute = {sh,-c,/usr/bin/php media/DiskID/home/username/www/username.server.feralhosting.com/public_html/rutorrent-1/php/initplugins.php username &}
+~~~
+
+`rtorrent` to `rtorrent-1`
+
+~~~
+scgi_local = media/DiskID/home/username/private/rtorrent-1/.socket
+~~~
+
+And then save the changes. in nano you would press and hold `CTRL` and then press `x`. Press `y` to confirm and exit
+
+### Edit the files - rutorrent
+
+Open your `~/www/username.feralhosting.com/public_html/rutorrent-1/conf/config.php`
+
+~~~
+nano -w ~/www/$(whoami).$(hostname)/public_html/rutorrent-1/conf/config.php
+~~~
+
+Edit this line, changing the paths to reflect the new locations.
+
+~~~
+$scgi_host = 'unix://'.$_pw['dir'].'/private/rtorrent/.socket';
+~~~
+
+For this example it is:
+
+~~~
+$scgi_host = 'unix://'.$_pw['dir'].'/private/rtorrent-1/.socket';
+~~~
+
+### Starting your new instance
+
+Load your custom instance in a new screen window.
+
+~~~
+screen -S rtorrent-1 rtorrent -n -o import=~/.rtorrent-1.rc
+~~~
+
+If you see the screen is terminating error, do this:
+
+~~~
+screen -S rtorrent-1
+~~~
+
+The do this:
+
+~~~
+rtorrent -n -o import=~/.rtorrent-1.rc
+~~~
+
+You will now see what the actual error is.
+
+Press and hold `CTRL` and `a`, then press `d` to detach from this screen session.
+
+### More instances
+
+Bascially follow this FAQ again, as many times as you want and just change the suffix.
+
+~~~
+rtorrent-2 and rutorrent-2
+rtorrent-3 and rutorrent-3
+rtorrent-4 and rutorrent-4
+rtorrent-5 and rutorrent-5
+rtorrent-6 and rutorrent-6
+~~~
+
+And so on. It is pretty much that simple.
+
+
+
---
+++ Feral Wiki/Software/Multiple instances - rtorrent and rutorrent/script/multirtru.sh
@@ -0,0 +1,148 @@
+#!/bin/bash
+# multi rtorrent/rutorrent
+scriptversion="1.0.0"
+scriptname="multirtru"
+# Author name
+#
+############################
+## Version History Starts ##
+############################
+#
+# How do I customise this updater? 
+# 1: scriptversion="0.0.0" replace "0.0.0" with your script version. This will be shown to the user at the current version.
+# 2: scriptname="multirtru" replace "multirtru" with your script name. this will be shown to the user when they first run the script.
+# 3: Search and replace all instances of "multirtru", 29 including this one, with the name of your script, do not include the .sh aside from doing step 2.
+# 4: Then replace ALL "https://raw.github.com/feralhosting" with the URL to the RAW script URL.
+# 5: Insert you script in the "Script goes here" labelled section 
+#
+# This updater deals with updating two files at the same time, the  "~/multirtru.sh" and the "~/bin/multirtru" . You can remove one part of the updater, if you wish, to focus on a single file instance.
+#
+############################
+### Version History Ends ###
+############################
+#
+#
+############################
+###### Variable Start ######
+############################
+#
+#
+#
+############################
+####### Variable End #######
+############################
+#
+############################
+#### Self Updater Start ####
+############################
+#
+mkdir -p $HOME/bin
+#
+if [ ! -f $HOME/multirtru.sh ]
+then
+    wget -qO $HOME/multirtru.sh https://raw.github.com/feralhosting
+fi
+if [ ! -f $HOME/bin/multirtru ]
+then
+    wget -qO $HOME/bin/multirtru https://raw.github.com/feralhosting
+fi
+#
+wget -qO $HOME/000multirtru.sh https://raw.github.com/feralhosting
+#
+if ! diff -q $HOME/000multirtru.sh $HOME/multirtru.sh > /dev/null 2>&1
+then
+    echo '#!/bin/bash
+    wget -qO $HOME/multirtru.sh https://raw.github.com/feralhosting
+    wget -qO $HOME/bin/multirtru https://raw.github.com/feralhosting
+    bash $HOME/multirtru.sh
+    exit 1' > $HOME/111multirtru.sh
+    bash $HOME/111multirtru.sh
+    exit 1
+fi
+if ! diff -q $HOME/000multirtru.sh $HOME/bin/multirtru > /dev/null 2>&1
+then
+    echo '#!/bin/bash
+    wget -qO $HOME/multirtru.sh https://raw.github.com/feralhosting
+    wget -qO $HOME/bin/multirtru https://raw.github.com/feralhosting
+    bash $HOME/multirtru.sh
+    exit 1' > $HOME/222multirtru.sh
+    bash $HOME/222multirtru.sh
+    exit 1
+fi
+#
+echo
+echo -e "Hello $(whoami), you have the latest version of the" "\033[36m""$scriptname""\e[0m" "script. This script version is:" "\033[31m""$scriptversion""\e[0m"
+echo
+#
+rm -f $HOME/000multirtru.sh $HOME/111multirtru.sh $HOME/222multirtru.sh
+chmod -f 700 $HOME/bin/multirtru
+#
+############################
+##### Self Updater End #####
+############################
+#
+read -ep "The scripts have been updated, do you wish to continue [y] or exit now [q] : " updatestatus
+echo
+if [[ $updatestatus =~ ^[Yy]$ ]]
+then
+#
+############################
+####### Script Start #######
+############################
+#
+echo -e "To do this we are going to clone the existing set-up and add a suffix to the end of it. For example:"
+echo
+echo -e "\033[32m""/public_html/rutorrent-1""\e[0m""," "\033[33m""~/.rtorrent-1.rc""\e[0m" "and" "\033[36m""~/private/rutorrent-1""\e[0m"
+echo
+echo -e "\033[31m""The first thing we need to do is pick a suffix to use:""\e[0m"
+read -ep "Please chose a suffix to use for our new rtorrent and rutorrent instances: " suffix
+echo
+if [[ -z "$suffix" ]]
+then
+    echo -e "\033[31m""You did not give a suffix to use. Please enter one. The script will restart""\e[0m"
+    echo
+    bash ~/multirtru.sh
+    exit 1
+else
+    if [[ ! -f ~/.rtorrent-$suffix.rc && ! -d ~/private/rutorrent-$suffix && ! -d ~/www/$(whoami).$(hostname)/public_html/rutorrent-$suffix ]]
+    then
+        # clone the installation
+        echo "1: Cloning the installation"
+        echo
+        cp -f ~/.rtorrent.rc ~/.rtorrent-$suffix.rc
+        mkdir -p ~/private/rtorrent-$suffix/data ~/private/rtorrent-$suffix/watch ~/private/rtorrent-$suffix/work
+        cp -rf ~/www/$(whoami).$(hostname)/public_html/rutorrent/. ~/www/$(whoami).$(hostname)/public_html/rutorrent-$suffix
+        # sed ~/.rtorrent-*.rc
+        echo "2 Part 1: Editing the files: rtorrent"
+        echo
+        sed -i 's|'$HOME'/private/rtorrent|'$HOME'/private/rtorrent-'$suffix'|g' ~/.rtorrent-$suffix.rc
+        sed -i 's|/public_html/rutorrent/php/initplugins.php|/public_html/rutorrent-'$suffix'/php/initplugins.php|g' ~/.rtorrent-$suffix.rc
+        # sed /rutorrent/
+        echo "2 Part 2: Editing the files: rutorrent"
+        echo
+        sed -i 's|/private/rtorrent/.socket|/private/rtorrent-'$suffix'/.socket|g' ~/www/$(whoami).$(hostname)/public_html/rutorrent-$suffix/conf/config.php
+        # create the screen
+        echo "3: Creating the screen process"
+        echo
+        screen -dmS rtorrent-$suffix rtorrent -n -o import=~/.rtorrent-$suffix.rc
+        echo "Done. To reattach to this screen type:" 
+        echo "screen -r rtorrent-$suffix"
+        echo
+        exit 1
+    else
+        echo -e "\033[31m""This particular suffix already exists, try another. The script will restart.""\e[0m"
+        echo
+        bash ~/multirtru.sh
+        exit 1
+    fi
+fi
+#
+############################
+####### Script Ends  #######
+############################
+#
+else
+    echo -e "You chose to exit after updating the scripts."
+    exit 1
+    cd && bash
+fi
